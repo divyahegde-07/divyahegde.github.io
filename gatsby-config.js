332282cbf19f6966b1ef299557d397f6360b0633
divyahegde-07 <92747781+divyahegde-07@users.noreply.github.com>
2025-11-08 19:24:58 -0500

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,12 +2,12 @@ const config = require('./src/config');
 
 module.exports = {
   siteMetadata: {
-    title: 'Brittany Chiang',
+    title: 'Divya Hegde',
     description:
-      'Brittany Chiang is a software engineer who specializes in building (and occasionally designing) exceptional digital experiences.',
-    siteUrl: 'https://brittanychiang.com', // No trailing slash allowed!
+      'Divya Hegde is a data scientist who specializes in analyzing data, building and maintaining models.',
+    siteUrl: 'https://divyahegde.netlify.app/', // No trailing slash allowed!
     image: '/og.png', // Path to your image you placed in the 'static' folder
-    twitterUsername: '@bchiang7',
+    twitterUsername: '',
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
