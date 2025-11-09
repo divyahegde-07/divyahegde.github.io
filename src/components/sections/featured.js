4f0f24fe5f56260ef75412467086b3936d6742cb
divyahegde-07 <92747781+divyahegde-07@users.noreply.github.com>
2025-11-08 21:00:58 -0500

    Update featured.js

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -256,7 +256,6 @@ const StyledProject = styled.li`
     a {
       width: 100%;
       height: 100%;
-      background-color: var(--green);
       border-radius: var(--border-radius);
       vertical-align: middle;
 
